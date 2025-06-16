9aece049ad349aab3055e776ac441d67f5a4fe75
Nguyễn Khánh Minh <nguyenkhanhminh251@gmail.com>
2025-06-16 14:59:45 +0700

    Set Up Repositories

---
+++ AirWaterStore.Data/Repositories/OrderDetailRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirWaterStore.Data.Repositories
+{
+    public class OrderDetailRepository : IOrderDetailRepository
+    {
+    }
+}
