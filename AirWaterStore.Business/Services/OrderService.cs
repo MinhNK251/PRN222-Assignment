a5c17dfa11ac458517c91c0ef1e84e4fe24433f9
Nguyễn Khánh Minh <nguyenkhanhminh251@gmail.com>
2025-06-16 14:59:29 +0700

    Set Up Services

---
+++ AirWaterStore.Business/Services/OrderService.cs
@@ -0,0 +1,13 @@
+﻿using AirWaterStore.Business.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirWaterStore.Data.Repositories
+{
+    public class OrderService : IOrderService
+    {
+    }
+}
