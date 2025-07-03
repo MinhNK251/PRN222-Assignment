8a53a6fef366dd8fbcf702395e9baa3cce072ec6
Jangkuz <longanh281@gmail.com>
2025-07-03 11:34:30 +0700

    [UPDATE] - Format currency to vnd

--- AirWaterStore.Web/Helper/Currency.cs
+++ AirWaterStore.Web/Helper/Currency.cs
@@ -5,14 +5,10 @@ namespace AirWaterStore.Web.Helper;
 
 public static class Currency
 {
-    public static CultureInfo VnCurrencyFormat()
-    {
-        string cultureCode = "vi-VN"; // Replace with the desired culture code
-        CultureInfo culture = new CultureInfo(cultureCode);
-
-        // Get the currency format information
-        // NumberFormatInfo currencyFormat = culture.NumberFormat;
-        return culture;
-    }
-
+    public const string CultureCode = "vi-VN";
+    public static readonly CultureInfo VnCurrencyFormat = new CultureInfo(CultureCode);
+    // public static string FormatCurrency(decimal amount)
+    // {
+    //     return amount.ToString("C", VnCurrencyFormat);
+    // }
 }
